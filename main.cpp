35c509aec51f6da18791d51f462e500312dc88c7
Ben Humphries <benfhumphries@gmail.com>
2019-01-04 16:16:04 -0500

    Added glm and matrices

--- main.cpp
+++ main.cpp
@@ -2,8 +2,14 @@
 
 #include <SDL2/SDL.h>
 #include <GL/glew.h>
+#include <glm/glm.hpp>
+
+#include <stdio.h>
 
 int main(){
+
+	const int WINDOW_WIDTH = 800;
+	const int WINDOW_HEIGHT = 600;
 
 	SDL_Init(SDL_INIT_VIDEO);
 
@@ -12,11 +18,20 @@ int main(){
 	SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 2);
 	SDL_GL_SetAttribute(SDL_GL_STENCIL_SIZE, 8);
 
-	SDL_Window * window = SDL_CreateWindow("OpenGL", 100, 100, 800, 600, SDL_WINDOW_OPENGL);
+	SDL_Window * window = SDL_CreateWindow("OpenGL", 100, 100, WINDOW_WIDTH, WINDOW_HEIGHT, SDL_WINDOW_OPENGL);
 	SDL_GLContext context = SDL_GL_CreateContext(window);
 
 	glewExperimental = GL_TRUE;
-	glewInit();
+	GLenum err = glewInit();
+	printf("%s\n", glewGetErrorString(err));
+
+
+	glm::mat4 view(1);
+	glm::mat4 proj(1);
+	glm::mat4 model(1);
+
+	view = glm::translate(view, glm::vec3(0,0,-3));
+	projection = glm::perspective(glm::radians(45.0f), WINDOW_WIDTH / WINDOW_HEIGHT, 0.1f, 300.0f);
 	
 	SDL_Event windowEvent;
 	while (true){
@@ -26,7 +41,6 @@ int main(){
 
 		GLuint vertexBuffer;
 		glGenBuffers(1, &vertexBuffer);
-
 		printf("%u\n", vertexBuffer);
 
 		SDL_GL_SwapWindow(window);
