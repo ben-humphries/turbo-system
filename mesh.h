966b47b977bf02d79a7ea903167ebf673ddb4a63
Paul Tilley <t1000.pixlark@gmail.com>
2019-01-07 20:15:51 -0500

    mesh.h and beginnings of ply-parser.cpp

---
+++ mesh.h
@@ -0,0 +1,10 @@
+#pragma once
+
+struct Mesh {
+	float * vertices;
+	size_t vertices_count;
+	int * indices;
+	size_t indices_count;
+	float * texture_coordinates;
+	size_t texture_coordinates_count;
+};
