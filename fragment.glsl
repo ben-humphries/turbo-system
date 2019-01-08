55f968c533c314f56473c505710a93a229a97d5f
Ben Humphries <benfhumphries@gmail.com>
2019-01-08 12:09:46 -0500

    Temp shader rewrite so models are easier to see

--- fragment.glsl
+++ fragment.glsl
@@ -1,7 +1,10 @@
 #version 330 core
 
+in vec2 texCoord;
+in float distance_from_zero;
+
 out vec4 fragColor;
 
 void main() {
-	fragColor = vec4(1.0, 1.0, 1.0, 1.0);
+	fragColor = vec4(0.5, 0.5, 1.0, 1.0) * distance_from_zero;
 }
--- vertex.glsl
+++ vertex.glsl
@@ -1,9 +1,16 @@
 #version 330 core
 
 in vec3 position;
+in vec2 _texCoord;
 
 uniform mat4 transform; // model/view/proj
 
+out vec2 texCoord;
+out float distance_from_zero;
+
 void main() {
-	gl_Position = transform * vec4(position, 1);
+
+	 distance_from_zero = 1 - position.y - position.x;
+	 texCoord = _texCoord;
+	 gl_Position = transform * vec4(position, 1);
 }
